f94ebf074e9ffbffa45dc11e448b992ec42ed9e4
jvb <jvb@bepark.be>
2016-03-08 10:19:28 +0100

    edit user

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,8 +2,7 @@
 
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
 
 /**
  * Defines application features from the specific context.
@@ -20,4 +19,21 @@ class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements
     public function __construct()
     {
     }
+
+    /**
+     * @Given I have a stored user
+     */
+    public function iHaveAStoredUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should be on the user edit page
+     */
+    public function iShouldBeOnTheUserEditPage()
+    {
+        throw new PendingException();
+    }
+
 }
--- features/user.feature
+++ features/user.feature
@@ -13,3 +13,11 @@ Feature: User list feature
 	When I follow "Add user"
 	Then I should be on "/application/user/add"
 	And the response status code should be 200
+
+  Scenario: The user list should have an edit button
+	Given I am on "/application/user"
+	And I have a stored user
+	When I follow "Edit user"
+	Then the url should match "/application/user/\d+"
+	And I should be on the user edit page
+	And the response status code should be 200
